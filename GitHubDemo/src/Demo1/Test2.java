600db5de37b05e158b0be41db347ad2a2fffbd88
Shree477 <supekarshree123@gmail.com>
2024-06-06 09:51:15 +0530

    Commit Test2

---
+++ GitHubDemo/src/Demo1/Test2.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Test 2 ");
+		System.out.println("One Line");
+		System.out.println("Two Line");
+	}
+
+}
